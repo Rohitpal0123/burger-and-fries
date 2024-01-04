c6df84b3a523549b118ba90384efcb99722bee8c
Rohitpal0123 <prohit9370@gmail.com>
2024-01-04 17:59:40 +0530

    Added axios lib

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const employeeRouter = require("./routes/employee");
 const managerRouter = require("./routes/manager");
 const roleRouter = require("./routes/role");
 const bulkUploadRouter = require("./routes/bulkUpload");
-const mealRouter = require("./routes/meal");
+const axiosRouter = require("./routes/axios");
 
 app.use("/product", productRouter);
 app.use("/category", categoryRouter);
@@ -39,7 +39,7 @@ app.use("/employee", employeeRouter);
 app.use("/manager", managerRouter);
 app.use("/role", roleRouter);
 app.use("/bulkUpload", bulkUploadRouter);
-app.use("/meal", mealRouter);
+app.use("/axios", axiosRouter);
 app.use("/", (req, res) => {
   res.send("Welcome to burger & fries");
 });
