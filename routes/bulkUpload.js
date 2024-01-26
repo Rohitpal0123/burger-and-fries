2ec16127d3d7b027c2ee911a99052e417a60e26e
Rohitpal0123 <prohit9370@gmail.com>
2024-01-26 11:48:16 +0530

    changed buluploads to memory storage

--- routes/bulkUpload.js
+++ routes/bulkUpload.js
@@ -5,20 +5,7 @@ const fs = require("fs");
 const { authenticateUser } = require("../middleware/authMiddleware");
 const router = express.Router();
 
-// Specify the directory to save uploaded files
-const uploadDir = path.join(__dirname, "../uploads");
-fs.existsSync(uploadDir) || fs.mkdirSync(uploadDir);
-
-// Configure Multer for handling file uploads and saving to disk
-const storage = multer.diskStorage({
-  destination: uploadDir,
-  filename: function(req, file, cb) {
-    const fileName = `${Date.now()}-${file.originalname}`;
-    cb(null, fileName);
-  }
-});
-
-const upload = multer({ storage: storage });
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.post(
   "/uploadProduct",
