da2cb9ea132c10327d101c59a9db7c1b63c71e73
Rohitpal0123 <prohit9370@gmail.com>
2024-01-26 09:21:14 +0530

    Added pre-commit hooks and winston logging

--- controllers/User/signup.js
+++ controllers/User/signup.js
@@ -1,6 +1,7 @@
 const UserVerification = require("../../models/userVerification.model");
 const User = require("../../models/user.model");
 const generateToken = require("./generateToken");
+const logger = require("../../lib/createFileLog");
 
 class signup {
   process = async (req, res) => {
@@ -8,7 +9,7 @@ class signup {
       const { userEmail, userOtp } = req.body;
 
       const userVerification = await UserVerification.findOne({
-        email: userEmail
+        email: userEmail,
       });
 
       let newUser;
@@ -24,15 +25,22 @@ class signup {
         userName: userVerification.userName,
         email: userVerification.email,
         password: userVerification.password,
-        role: userVerification.role
+        role: userVerification.role,
       });
 
       await UserVerification.deleteOne({ _id: userVerification._id });
+
+      const childLogger = logger.child({
+        email: newUser.email,
+        role: newUser.role,
+      });
+      childLogger.log("info", `Successful login by ${newUser.userName}`);
+
       res.status(200).json({
         _id: newUser._id,
         email: newUser.email,
         role: newUser.role,
-        token: generateToken(newUser._id, newUser.role)
+        token: generateToken(newUser._id, newUser.role),
       });
     } catch (error) {
       res.status(400).json(error);
