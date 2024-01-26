077bd19d5d0cea7b6a389251ca51ab217c6fbe62
Rohitpal0123 <prohit9370@gmail.com>
2024-01-26 13:57:57 +0530

    still redis config

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ require("dotenv").config();
 const redisClient = Redis.createClient({
   socket: {
     host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD
   }
 });
 redisClient.on("error", (error) => console.log("Redis Error:", error));
